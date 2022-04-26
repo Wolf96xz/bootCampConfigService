22f941787bbbbd4d66ca99ef5cb47fe5d236e65e
Toni Burgos <burgosantonio.burgos1@gmail.com>
2022-04-26 13:40:12 +0200

    Proyecto microservicios

---
+++ adminserver/src/main/java/com/nttdata/adminservice/AdminserviceApplication.java
@@ -0,0 +1,15 @@
+package com.nttdata.adminservice;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@EnableAdminServer
+@SpringBootApplication
+public class AdminserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminserviceApplication.class, args);
+	}
+
+}
---
+++ gateway/src/test/java/com/nttdata/gateway/GatewayaApplicationTests.java
@@ -0,0 +1,15 @@
+package com.nttdata.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@EnableEurekaClient
+@SpringBootTest
+class GatewayaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
